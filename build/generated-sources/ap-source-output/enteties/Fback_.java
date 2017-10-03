a857b5ebc92e33068ff445ed3e8fc9a3ec016bc7
eivimoe <31846925+eivimoe@users.noreply.github.com>
2017-10-03 14:54:59 +0200

    feedback fix

---
+++ build/generated-sources/ap-source-output/enteties/Fback_.java
@@ -0,0 +1,17 @@
+package enteties;
+
+import enteties.UserE;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-03T14:26:45")
+@StaticMetamodel(Fback.class)
+public class Fback_ { 
+
+    public static volatile SingularAttribute<Fback, Integer> rating;
+    public static volatile SingularAttribute<Fback, String> feedbackMessage;
+    public static volatile SingularAttribute<Fback, Long> id;
+    public static volatile SingularAttribute<Fback, UserE> user;
+
+}
--- build/generated-sources/ap-source-output/enteties/UserE_.java
+++ build/generated-sources/ap-source-output/enteties/UserE_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-03T13:35:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-03T14:26:45")
 @StaticMetamodel(UserE.class)
 public class UserE_ { 
 
